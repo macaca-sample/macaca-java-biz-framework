510d9dea27bf449705f2ba65929a5cce1c9abe1b
shixing <18537159533@139.com>
2020-05-18 17:33:34 +0800

    1. 删除没用的类 2. 新增公共类 AppCommonPage.java & WebCommonPage.java 3. 增加CI文件 4. 修改pom，主要更新了JUnit版本，maven版本。

--- src/test/java/macaca/java/biz/AppTest.java
+++ src/test/java/macaca/java/biz/AppTest.java
@@ -1,34 +1,15 @@
 package macaca.java.biz;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+public class AppTest {
+    @Test
+    public void appTest(){
+        Assertions.assertTrue(true);
     }
 }
